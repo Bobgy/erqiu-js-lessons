4b9c35c3ada727652a9949d709cd63eb9913c3a5
Yuan Gong <gongyuan94@gmail.com>
2018-10-02 03:33:30 +0900

    refactor cypress tests, include coverage for randomize button

--- cypress/integration/erqiu-js-lessons/lesson-1-sort.spec.js
+++ cypress/integration/erqiu-js-lessons/lesson-1-sort.spec.js
@@ -1,22 +1,24 @@
 /// <reference types="Cypress" />
 
-function arrayItemNumbersShould(assertFn) {
-    return cy.get('.array-item-number')
-        .should(($numbers) => {
-            const texts = $numbers.map((i, el) => Cypress.$(el).text()).get()
-            assertFn(texts)
-        })
+const accum = (...cmds) => {
+  const results = []
+
+  cmds.forEach((cmd) => {
+    cmd().then(results.push.bind(results))
+  })
+
+  return cy.wrap(results)
 }
 
 context('lesson-1-sort', () => {
     beforeEach(() => {
         cy.visit('http://localhost:3000')
-        
+
         cy.get('button').contains('Using erqiu\'s algorithm')
             .click()
             .get('button').contains('Using yuan\'s algorithm')
             .should('exist')
-        
+
         cy.get('label').contains('Speed:')
             .children().first()
             .select('fastest')
@@ -25,26 +27,56 @@ context('lesson-1-sort', () => {
     it('sorts correctly', () => {
         cy.get('#lesson-1-sort-container')
             .should('exist')
-        
-        arrayItemNumbersShould((texts) => {
-            expect(texts).length.lessThan(15)
-            expect(texts).length.greaterThan(4)
-            texts.forEach((text) => {
-                expect(Number(text), `number parsed from "${text}"`).to.not.be.NaN
+
+        verifyArrayNumbers()
+        const cmdClickRandomize = () => cy.get('button').contains('Randomize').click()
+
+        accum(findArrayItemNumbers, cmdClickRandomize, findArrayItemNumbers)
+            .then(([numbersBefore, _, numbersAfter]) => {
+                expect(numbersBefore).to.not.deep.equal(numbersAfter)
             })
-        })
 
-        cy.get('button').contains('Start')
-            .click()
+        startAlgorithm()
 
-        cy.get('h4').contains('Status: complete')
-        arrayItemNumbersShould((texts) => {
-            const arrayOfNumbers = texts.map(text => Number(text))
-            arrayOfNumbers.forEach((num, index) => {
+        waitAlgorithmComplete()
+
+        findArrayItemNumbers().then((numbers) => {
+            numbers.forEach((num, index) => {
                 if (index > 0) {
-                    expect(arrayOfNumbers[index - 1]).lte(num, 'adjacent pairs should be in order')
+                    expect(numbers[index - 1]).lte(num, 'adjacent pairs should be in order')
                 }
             })
         })
     })
+
 })
+
+function findArrayItemNumbers() {
+    return cy.get('.array-item-number')
+        .then(($numbers) => {
+            const numbers = $numbers.map((i, el) => Cypress.$(el).text()).get()
+                .map(Number).slice()
+            console.log(numbers)
+
+            return numbers
+        })
+}
+
+function verifyArrayNumbers() {
+    findArrayItemNumbers().then(numbers => {
+        expect(numbers).length.lessThan(15)
+        expect(numbers).length.greaterThan(4)
+        numbers.forEach((number, index) => {
+            expect(number, `number at ${index}`).to.not.be.NaN
+        })
+    })
+}
+
+function startAlgorithm() {
+    cy.get('button').contains('Start')
+        .click()
+}
+
+function waitAlgorithmComplete() {
+    cy.get('h4').contains('Status: complete')
+}
