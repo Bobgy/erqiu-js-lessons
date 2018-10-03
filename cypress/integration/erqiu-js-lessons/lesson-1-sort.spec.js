a123fd7f74821937dc11118f08b0face098824d0
Yuan Gong <yuan@indeed.com>
2018-10-03 21:23:47 +0900

    implement more integration tests to cover all functionalities

--- cypress/integration/erqiu-js-lessons/lesson-1-sort.spec.js
+++ cypress/integration/erqiu-js-lessons/lesson-1-sort.spec.js
@@ -19,9 +19,7 @@ context('lesson-1-sort', () => {
             .get('button').contains('Using yuan\'s algorithm')
             .should('exist')
 
-        cy.get('label').contains('Speed:')
-            .children().first()
-            .select('fastest')
+        selectSpeed('fastest')
     })
 
     it('sorts correctly', () => {
@@ -29,27 +27,77 @@ context('lesson-1-sort', () => {
             .should('exist')
 
         verifyArrayNumbers()
-        const cmdClickRandomize = () => cy.get('button').contains('Randomize').click()
 
-        accum(findArrayItemNumbers, cmdClickRandomize, findArrayItemNumbers)
+        let numbersAfterRandomization
+        accum(findArrayItemNumbers, clickRandomizeButton, findArrayItemNumbers)
             .then(([numbersBefore, _, numbersAfter]) => {
+                numbersAfterRandomization = numbersAfter
                 expect(numbersBefore).to.not.deep.equal(numbersAfter)
             })
 
         startAlgorithm()
-
         waitAlgorithmComplete()
+        verifySorted()
 
+        cy.log('After resetting, all numbers are in the same order as initial')
+        cy.get('button').contains('Reset').click()
+        waitAlgorithmStatus('initial')
         findArrayItemNumbers().then((numbers) => {
-            numbers.forEach((num, index) => {
-                if (index > 0) {
-                    expect(numbers[index - 1]).lte(num, 'adjacent pairs should be in order')
-                }
-            })
+            expect(numbers).to.deep.equal(numbersAfterRandomization)
         })
     })
 
+    it('pause/resume works', () => {
+        selectSpeed('fast')
+        startAlgorithm()
+        cy.wait(1000)
+
+        accum(
+            findArrayItemNumbers,
+            () => cy.wait(1000).get('button').contains('Pause').click(),
+            () => waitAlgorithmStatus('paused'),
+            () => cy.wait(1000),
+            findArrayItemNumbers
+        ).then(([numbersBefore, _1, _2, _3, numbersAfter]) => {
+            expect(numbersBefore).to.not.deep.equal(numbersAfter)
+        })
+
+        selectSpeed('fastest')
+        cy.get('button').contains('Resume').click()
+        waitAlgorithmComplete()
+        verifySorted()
+    })
+
+    it('can generate distinct number sequence', () => {
+        cy.get('label').contains('Allow Duplicate Number').children().first().click()
+        clickRandomizeButton()
+        findArrayItemNumbers().then(numbers => {
+            const setOfNumbers = new Set(numbers)
+            // no duplicate numbers
+            expect(numbers).to.have.lengthOf(setOfNumbers.size)
+        })
+    })
 })
+
+function clickRandomizeButton() {
+    return cy.get('button').contains('Randomize').click()
+}
+
+function verifySorted() {
+    return findArrayItemNumbers().then((numbers) => {
+        numbers.forEach((num, index) => {
+            if (index > 0) {
+                expect(numbers[index - 1]).lte(num, 'adjacent pairs should be in order')
+            }
+        })
+    })
+}
+
+function selectSpeed(speed = 'normal') {
+    return cy.get('label').contains('Speed:')
+        .children().first()
+        .select(speed)
+}
 
 function findArrayItemNumbers() {
     return cy.get('.array-item-number')
@@ -63,7 +111,7 @@ function findArrayItemNumbers() {
 }
 
 function verifyArrayNumbers() {
-    findArrayItemNumbers().then(numbers => {
+    return findArrayItemNumbers().then(numbers => {
         expect(numbers).length.lessThan(15)
         expect(numbers).length.greaterThan(4)
         numbers.forEach((number, index) => {
@@ -73,10 +121,14 @@ function verifyArrayNumbers() {
 }
 
 function startAlgorithm() {
-    cy.get('button').contains('Start')
+    return cy.get('button').contains('Start')
         .click()
 }
 
 function waitAlgorithmComplete() {
-    cy.get('h4').contains('Status: complete')
+    return waitAlgorithmStatus('complete')
+}
+
+function waitAlgorithmStatus(goalStatus) {
+    return cy.get('h4').contains(`Status: ${goalStatus}`)
 }
