3682c32cbf436ebd295d6706271345463927355e
Yuan Gong <yuan@indeed.com>
2018-10-31 01:41:47 +0900

    update

---
+++ src/lesson-1-sort/erqiu/bubbleSort.ts
@@ -0,0 +1,13 @@
+import { CompareAction, SwapAction } from '../yuan/commonTypes'
+
+async function bubbleSort(length: number, isLessThan: CompareAction, swap: SwapAction) {
+  // write your code here
+  // for comparing
+  //
+  // const compareResult = await isLessThan(i, j);
+  // for swapping items
+  //
+  // await swap(i, j)
+}
+
+export default bubbleSort
