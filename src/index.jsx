7efd4068f13beea378ff77689f1662220e2767a6
Yuan Gong <yuan@indeed.com>
2018-11-10 21:09:52 +0900

    remove unneeded api

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,7 @@
-import React, { ConcurrentMode } from 'react'
+import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
 
 // ReactDOM.render(<App />, document.getElementById('root'))
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <ConcurrentMode>
-    <App />
-  </ConcurrentMode>,
-)
+ReactDOM.createRoot(document.getElementById('root')).render(<App />)
