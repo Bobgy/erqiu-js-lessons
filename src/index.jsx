99e9cebcb056e522632b6ac6aa15c666ec3c498c
Yuan Gong <yuan@indeed.com>
2018-11-10 21:05:50 +0900

    enable react concurrent mode

---
+++ src/index.jsx
@@ -0,0 +1,11 @@
+import React, { ConcurrentMode } from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
+
+// ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <ConcurrentMode>
+    <App />
+  </ConcurrentMode>,
+)
